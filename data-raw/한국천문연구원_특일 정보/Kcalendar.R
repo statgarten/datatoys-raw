1af6afb4c6a463db998aee6c56ce4df841ecd6c6
Youngjun Na <youngjunna@Youngjunui-MacBookPro.local>
2023-09-26 17:30:37 +0900

    add data-raw

---
+++ data-raw/한국천문연구원_특일 정보/Kcalendar.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+library(lubridate)
+
+Kcalendar <- read_csv("data-raw/한국천문연구원_특일 정보/korea-calendar.csv")
+
+glimpse(Kcalendar)
+
+Kcalendar <- Kcalendar %>%
+  mutate(
+    손있는날 = gsub(",", "", 손있는날)
+  )
+
+usethis::use_data(Kcalendar, overwrite = TRUE)
